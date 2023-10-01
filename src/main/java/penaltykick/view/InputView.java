0f9bed454c52463ced716e9792cb0c73a77e736f
yuseonjun <seonjun0906@gmail.com>
2023-10-01 17:04:02 +0900

    feat: 숫자가 아닌 값이 들어오면 IllegalArgumentException 을 발생한다.

--- src/main/java/penaltykick/view/InputView.java
+++ src/main/java/penaltykick/view/InputView.java
@@ -9,15 +9,23 @@ public class InputView {
 	private InputView() {
 	}
 
-	public static String readPlayerNumber() {
+	public static int readPlayerNumber() {
 		String readNumber = scanner.next();
 		validateBlank(readNumber);
-		return readNumber;
+		return validateInteger(readNumber);
 	}
 
 	private static void validateBlank(String input) {
 		if (input == null || input.isBlank()) {
 			throw new IllegalArgumentException("반칸이 입력될 수 없습니다.");
+		}
+	}
+
+	private static int validateInteger(String input) {
+		try {
+			return Integer.parseInt(input);
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException("숫자가 아닌 값은 입력될 수 없습니다.");
 		}
 	}
 }
