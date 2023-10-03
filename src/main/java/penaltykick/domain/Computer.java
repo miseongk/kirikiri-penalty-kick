b4053036c1c8a5ff6f0e12ae38b959094585080d
yuseonjun <seonjun0906@gmail.com>
2023-10-03 19:06:22 +0900

    feat: 컴퓨터 숫자를 생성할때 1에서 3사이의 숫자만 생성되야 한다.

--- src/main/java/penaltykick/domain/Computer.java
+++ src/main/java/penaltykick/domain/Computer.java
@@ -4,10 +4,19 @@ import java.util.List;
 
 public class Computer {
 
+	private static final int MAX_COMPUTER_NUMBER = 3;
+
 	private final List<Integer> computerNumbers;
 
 	public Computer(List<Integer> computerNumbers) {
+		validateComputerNumber(computerNumbers);
 		this.computerNumbers = computerNumbers;
+	}
+
+	private void validateComputerNumber(List<Integer> computerNumbers) {
+		if (computerNumbers.stream().anyMatch(number -> number > MAX_COMPUTER_NUMBER)) {
+			throw new IllegalArgumentException("컴퓨터 숫자 생성에 오류가 발생하였습니다.");
+		}
 	}
 
 	public boolean checkNumberByPlayer(int playerNumber, int i) {
