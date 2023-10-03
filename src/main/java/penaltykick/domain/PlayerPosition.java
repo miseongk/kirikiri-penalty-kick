f227874f543e1f1ab272a98504839b99884616de
Hugh <lee2317884@gmail.com>
2023-10-04 00:55:06 +0900

    fix: 사용되지 않는 import 제거

--- src/main/java/penaltykick/domain/PlayerPosition.java
+++ src/main/java/penaltykick/domain/PlayerPosition.java
@@ -2,7 +2,6 @@ package penaltykick.domain;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.StringTokenizer;
 import java.util.stream.Collectors;
 
 public class PlayerPosition {
