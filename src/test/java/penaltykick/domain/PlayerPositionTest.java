c98faecd67327e2ea4a971b709afcaa232c8ff9b
Hugh <lee2317884@gmail.com>
2023-10-04 00:51:53 +0900

    test: 플레이어 위치 입력 받기 테스트

---
+++ src/test/java/penaltykick/domain/PlayerPositionTest.java
@@ -0,0 +1,16 @@
+package penaltykick.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PlayerPositionTest {
+
+	@Test
+	void 입력_받는지_테스트() {
+		String input = "1 3 2 2 1";
+		int[] pos = {1, 3, 2, 2, 1};
+		int[] playerPos = new PlayerPosition(input).getPlayerPosition().stream().mapToInt(num -> num).toArray();
+		assertArrayEquals(pos, playerPos);
+	}
+}
