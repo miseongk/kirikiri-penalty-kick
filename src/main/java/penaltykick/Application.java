c46654fdfa89c6d55f60178aaac9134c5dbb1d7e
Chaeros <chpark6851@koreatech.ac.kr>
2023-10-04 23:47:53 +0900

    1. 시작 문구 출력

--- src/main/java/penaltykick/Application.java
+++ src/main/java/penaltykick/Application.java
@@ -1,8 +1,21 @@
 package penaltykick;
 
-public class Application {
+import java.io.*;
 
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+public class Application {
+    static int playerNum=2; // 총 플레이어 수
+    static int shootingNum=5; // 각 플레이어의 슈팅 횟수
+
+    // 시작 출력문구
+    static void printOpeningPhrase(){
+        System.out.println("패널티 킥 게임을 시작합니다.");
+        System.out.println("총 "+shootingNum+"번의 위치를 입력해주세요.");
+    }
+
+    public static void main(String[] args) throws IOException{
+
+        // 시작문구 출력
+        printOpeningPhrase();
+
     }
 }
