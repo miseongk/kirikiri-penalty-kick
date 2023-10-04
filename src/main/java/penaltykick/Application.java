a455f3334c3db5c72fe75fc2f1bbee501ced2348
Chaeros <chpark6851@koreatech.ac.kr>
2023-10-04 23:52:22 +0900

    4. 랜덤 골키퍼 방향 설정

--- src/main/java/penaltykick/Application.java
+++ src/main/java/penaltykick/Application.java
@@ -1,12 +1,14 @@
 package penaltykick;
 
 import java.io.*;
+import java.util.Random;
 import java.util.StringTokenizer;
 
 public class Application {
     static int playerNum=2; // 총 플레이어 수
     static int shootingNum=5; // 각 플레이어의 슈팅 횟수
     static int playerKick[][]; // 플레이어가 슈팅할 방향 저장
+    static int kipperDirection[]; // 키퍼가 막을 방향 저장
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     // 시작 출력문구
@@ -36,16 +38,28 @@ public class Application {
             if(val!=1 && val!=2 && val!=3) throw new IllegalArgumentException("잘못된 슈팅 방향이 입력되었습니다.");
         }
     }
+
+    // 랜덤 골키퍼 방향 설정
+    static void configKipperDirection(){
+        Random rd = new Random();
+        for(int i=0;i<shootingNum;++i){
+            kipperDirection[i]=(rd.nextInt(3)+1);
+        }
+    }
     public static void main(String[] args) throws IOException{
 
         // 플레이어 슈팅 방향 및 키퍼 선방 방향 값을 담은 배열 크기 설정
         playerKick = new int[playerNum][shootingNum];
+        kipperDirection = new int[shootingNum];
 
         // 시작문구 출력
         printOpeningPhrase();
 
         // 플레이어 슈팅 방향 입력
         for(int i=1;i<=playerNum;++i) insertShooting(i);
+
+        // 랜덤 골키퍼 방향 설정
+        configKipperDirection();
 
     }
 }
