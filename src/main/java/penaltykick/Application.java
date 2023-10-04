2922a359322d0d4f3644b797441e752a9264fd1b
kkonii <119216472+kkonii@users.noreply.github.com>
2023-10-04 16:14:42 +0900

    Feat : 게임 시작 안내 문구를 출력

--- src/main/java/penaltykick/Application.java
+++ src/main/java/penaltykick/Application.java
@@ -1,8 +1,16 @@
 package penaltykick;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Application {
+    static String regex = "";
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        // 안내 문구
+        System.out.println("패널티 킥 게임을 시작합니다.");
+        System.out.println("총 5번의 위치를 입력해주세요.");
     }
 }
