fe5eca5d0e593c78b00711e125729e07827b589a
kkonii <119216472+kkonii@users.noreply.github.com>
2023-10-04 19:34:11 +0900

    Feat : 플레이어가 1-3사이의 값을 입력하지 않았을 경우 예외문구 출력

--- src/main/java/penaltykick/Application.java
+++ src/main/java/penaltykick/Application.java
@@ -11,8 +11,17 @@ public class Application {
         if (!player.replaceAll(" ", "").matches("^[0-9]*$")) {
             throw new IllegalArgumentException("공백으로 구분해서 입력해야 합니다.");
         }
-        if (player.split(" ").length != 5) {
+
+        String[] numbers = player.split(" ");
+
+        if (numbers.length != 5) {
             throw new IllegalArgumentException("5개의 값을 입력해야 합니다.");
+        }
+
+        for (String number : numbers) {
+            if (Integer.valueOf(number) < 1 || Integer.valueOf(number) > 3) {
+                throw new IllegalArgumentException("1-3 사이의 값만 입력할 수 있습니다.");
+            }
         }
     }
 
