26c90f37a1148559d9b92d307f67e92a7c22b8e7
HongYeseul <ghd8119@naver.com>
2023-10-06 06:22:22 +0900

    feat: GoalKepper(컴퓨터) 5개 위치 랜덤 선택(생성자 사용)

--- src/main/java/penaltykick/GoalKeeper.java
+++ src/main/java/penaltykick/GoalKeeper.java
@@ -1,4 +1,13 @@
 package penaltykick;
 
-public class GoalKeeper {
+public class GoalKeeper { // Computer
+    private int[] computer = new int[5];
+
+    public GoalKeeper(){
+        for (int i = 0; i < 5; i++) {
+            computer[i] = (int)(Math.random()*2)+1;
+        }
+    }
+
+
 }
