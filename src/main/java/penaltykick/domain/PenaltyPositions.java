c71109473483ccc7b1807e663898bc3930320be3
nhlee98 <e5slnh9876@gmail.com>
2023-10-06 00:02:20 +0900

    feat: 두 골대의 위치가 같은지 비교하는 기능 구현

--- src/main/java/penaltykick/domain/PenaltyPositions.java
+++ src/main/java/penaltykick/domain/PenaltyPositions.java
@@ -2,6 +2,8 @@ package penaltykick.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public final class PenaltyPositions {
     private static final int ATTEMPTS_COUNT = 5;
@@ -17,5 +19,15 @@ public final class PenaltyPositions {
         if (selectedPositions.size() != ATTEMPTS_COUNT) {
             throw new IllegalArgumentException(String.format("[ERROR] 패널티킥은 총 %d번의 시도여야 합니다.", ATTEMPTS_COUNT));
         }
+    }
+
+    public List<Boolean> matchPositions(PenaltyPositions penaltyPositions) {
+        return IntStream.range(0, ATTEMPTS_COUNT)
+                .mapToObj(turn -> this.get(turn) == penaltyPositions.get(turn))
+                .collect(Collectors.toList());
+    }
+
+    private PostPosition get(int index) {
+        return selectedPositions.get(index);
     }
 }
