30e1e8dc064d7670ef0d58eca1a0848adc145675
ParkDH0809 <znxn1237@gmail.com>
2023-10-06 00:03:34 +0900

    feat(service): 숫자 리스트 비교 후 결과 문자열 반환 구현

--- src/main/java/penaltykick/service/PenaltykickService.java
+++ src/main/java/penaltykick/service/PenaltykickService.java
@@ -2,6 +2,7 @@ package penaltykick.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class PenaltykickService {
     private static final int KICK_NUMBER = 5;
@@ -11,5 +12,17 @@ public class PenaltykickService {
             randomList.add((int)(Math.random() * 3) + 1);
         }
         return randomList;
+    }
+
+    public static String makeResultString(List<Integer> List1, List<Integer> List2) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for(int i = 0; i< KICK_NUMBER; i++) {
+            if(Objects.equals(List1.get(i), List2.get(i))) {
+                stringBuilder.append("O");
+                continue;
+            }
+            stringBuilder.append("X");
+        }
+        return stringBuilder.toString();
     }
 }
