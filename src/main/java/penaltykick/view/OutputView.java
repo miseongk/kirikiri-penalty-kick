1dfc50c35ec00687be069af0d33f31f26bdff560
ParkDH0809 <znxn1237@gmail.com>
2023-10-06 00:29:54 +0900

    feat(OutputView): 게임 결과 출력 문구 구현

--- src/main/java/penaltykick/view/OutputView.java
+++ src/main/java/penaltykick/view/OutputView.java
@@ -6,6 +6,9 @@ public class OutputView {
     private static final String PRINT_GAME_RESULT = "\n게임 결과";
     private static final String PRINT_PLAYER_1_RESULT = "플레이어1 : " ;
     private static final String PRINT_PLAYER_2_RESULT = "플레이어2 : " ;
+    private static final String PRINT_RESULT_PLAYER_1_WIN = "플레이어1이(가) 우승했습니다\uD83C\uDF89";
+    private static final String PRINT_RESULT_PLAYER_2_WIN = "플레이어2이(가) 우승했습니다\uD83C\uDF89";
+    private static final String PRINT_RESULT_DRAW = "무승부입니다.";
     public void printStartGame() {
         System.out.println(PRINT_START_GAME);
     }
@@ -24,5 +27,17 @@ public class OutputView {
 
     public void printPlayer2Result(String result) {
         System.out.println(PRINT_PLAYER_2_RESULT + result);
+    }
+
+    public void printResultPlayer1Win() {
+        System.out.println(PRINT_RESULT_PLAYER_1_WIN);
+    }
+
+    public void printResultPlayer2Win() {
+        System.out.println(PRINT_RESULT_PLAYER_2_WIN);
+    }
+
+    public void printResultDraw() {
+        System.out.println(PRINT_RESULT_DRAW);
     }
 }
