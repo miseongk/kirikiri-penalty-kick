fa4c591e86a11e3e6b68d16b3f90ac30f7e16255
HongYeseul <ghd8119@naver.com>
2023-10-06 06:31:40 +0900

    feat: Player 객체 생성 - 사용자에게 입력받아 player[] 초기화 - 적정 값 입력되지 않을 시 IllegalArgumentException 발생

--- src/main/java/penaltykick/Player.java
+++ src/main/java/penaltykick/Player.java
@@ -1,4 +1,23 @@
 package penaltykick;
 
+import java.util.StringTokenizer;
+
 public class Player {
+    int[] player = new int[5];
+
+    // player에게 입력받아 player[] 초기화
+    public void playerInput(String input){
+        StringTokenizer st =  new StringTokenizer(input);
+
+        for(int i=0; i<5; i++){
+            player[i] = Integer.parseInt(st.nextToken());
+            checkInput(player[i]);
+        }
+    }
+    // 적정 값 입력되지 않을 경우 IllegalArgumentException 발생
+    void checkInput(int input){
+        if ( input > 3 || input < 1) {
+            throw new IllegalArgumentException("입력 값은 1~3의 숫자입니다.");
+        }
+    }
 }
