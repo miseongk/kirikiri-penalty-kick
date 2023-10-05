02109b64b57bbaf984a8e3851b1bfe1fab308b82
ParkDH0809 <znxn1237@gmail.com>
2023-10-06 00:24:00 +0900

    style(OutputView): 게임 결과 문장 개행 추가

--- src/main/java/penaltykick/view/OutputView.java
+++ src/main/java/penaltykick/view/OutputView.java
@@ -3,7 +3,7 @@ package penaltykick.view;
 public class OutputView {
     private static final String PRINT_START_GAME = "패널티 킥 게임을 시작합니다.";
     private static final String PRINT_INPUT_FIVE_NUMBER = "총 5번의 위치를 입력해주세요.";
-    private static final String PRINT_GAME_RESULT = "게임 결과";
+    private static final String PRINT_GAME_RESULT = "\n게임 결과";
     private static final String PRINT_PLAYER_1_RESULT = "플레이어1 : " ;
     private static final String PRINT_PLAYER_2_RESULT = "플레이어2 : " ;
     public void printStartGame() {
