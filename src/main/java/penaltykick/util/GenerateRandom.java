5ea6101e3508a25c2f5210254064c7d111b71efa
jongheonleee <qwefghnm1212@gmail.com>
2023-10-05 11:36:45 +0900

    난수 생성기 객체 구현

---
+++ src/main/java/penaltykick/util/GenerateRandom.java
@@ -0,0 +1,18 @@
+package penaltykick.util;
+
+import java.util.Random;
+
+public class GenerateRandom {
+
+    private final Random random;
+    private final Integer START = 1;
+    private final Integer END = 3;
+
+    public GenerateRandom() {
+        random = new Random();
+    }
+    public int getNumber() {
+        return START + random.nextInt(END);
+    }
+
+}
