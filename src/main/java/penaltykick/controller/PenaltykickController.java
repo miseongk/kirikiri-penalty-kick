0ef5347102f4c022be322ddfc88960ed0d412558
ParkDH0809 <znxn1237@gmail.com>
2023-10-06 00:17:03 +0900

    feat(controller): 각 플레이어 컴퓨터와 숫자 비교, 결과 저장 동작 구현

--- src/main/java/penaltykick/controller/PenaltykickController.java
+++ src/main/java/penaltykick/controller/PenaltykickController.java
@@ -1,8 +1,11 @@
 package penaltykick.controller;
 
 import penaltykick.model.Player;
+import penaltykick.service.PenaltykickService;
 import penaltykick.view.InputView;
 import penaltykick.view.OutputView;
+
+import java.util.List;
 
 public class PenaltykickController {
     InputView inputView = new InputView();
@@ -13,8 +16,19 @@ public class PenaltykickController {
         try {
             Player player1 = new Player(inputView.inputNumberPlayer1());
             Player player2 = new Player(inputView.inputNumberPlayer2());
+            comparePlayer(player1, player2);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    public void comparePlayer(Player player1, Player player2) {
+        List<Integer> computerList = PenaltykickService.makeRandomNumberList();
+        compareEachPlayer(computerList, player1);
+        compareEachPlayer(computerList, player2);
+    }
+
+    public void compareEachPlayer(List<Integer> computerList, Player player) {
+        player.initResultValue(PenaltykickService.makeResultString(computerList, player.getNumberList()));
     }
 }
