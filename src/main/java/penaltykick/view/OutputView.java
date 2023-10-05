8cd65a9774f0901ba88dc2ad461bf3c2d9788f4a
ParkDH0809 <znxn1237@gmail.com>
2023-10-05 23:09:20 +0900

    feat(OutputView): 게임 시작 출력 문구 구현

---
+++ src/main/java/penaltykick/view/OutputView.java
@@ -0,0 +1,9 @@
+package penaltykick.view;
+
+public class OutputView {
+    private static final String PRINT_START_GAME = "패널티 킥 게임을 시작합니다.";
+
+    public void printStartGame() {
+        System.out.println(PRINT_START_GAME);
+    }
+}
