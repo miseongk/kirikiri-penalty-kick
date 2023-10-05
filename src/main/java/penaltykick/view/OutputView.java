e65f9d35591053e0855b6787b38df8d007a401c8
ParkDH0809 <znxn1237@gmail.com>
2023-10-05 23:13:28 +0900

    feat(OutputView): 설명 출력 문구 구현

--- src/main/java/penaltykick/view/OutputView.java
+++ src/main/java/penaltykick/view/OutputView.java
@@ -2,8 +2,13 @@ package penaltykick.view;
 
 public class OutputView {
     private static final String PRINT_START_GAME = "패널티 킥 게임을 시작합니다.";
+    private static final String PRINT_INPUT_FIVE_NUMBER = "총 5번의 위치를 입력해주세요.";
 
     public void printStartGame() {
         System.out.println(PRINT_START_GAME);
+    }
+
+    public void printInputFiveNumber() {
+        System.out.println(PRINT_INPUT_FIVE_NUMBER);
     }
 }
