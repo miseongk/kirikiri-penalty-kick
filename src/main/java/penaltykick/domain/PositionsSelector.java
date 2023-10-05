ed084d50c488f0bde43937b394ebfb3ddf4cd286
nhlee98 <e5slnh9876@gmail.com>
2023-10-06 01:18:35 +0900

    refactor: 랜덤으로 골대 위치 선택시 리스트가 아닌 일급 컬렉션을 반환하도록 변경
    PositionsSelector 클래스의 selectPositions 메소드 반환 타입을 PenaltyPositions 로 변경한다.

--- src/main/java/penaltykick/domain/PositionsSelector.java
+++ src/main/java/penaltykick/domain/PositionsSelector.java
@@ -8,9 +8,14 @@ import java.util.stream.IntStream;
 public class PositionsSelector {
     private static final Random RANDOM = new Random();
     private static final PostPosition[] POSITIONS = PostPosition.values();
+
     private PositionsSelector() {}
 
-    public static List<PostPosition> selectPositions(int count) {
+    public static PenaltyPositions selectPositions(int count) {
+        return new PenaltyPositions(selectRandomPositions(count));
+    }
+    
+    private static List<PostPosition> selectRandomPositions(int count) {
         return IntStream.range(0, count)
                 .map(i -> RANDOM.nextInt(POSITIONS.length))
                 .mapToObj(pick -> POSITIONS[pick])
