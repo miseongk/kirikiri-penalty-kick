d23fb82b621e66d4cf4772adbf62e690b00ab693
Hugh <lee2317884@gmail.com>
2023-10-05 18:10:02 +0900

    feat: 골 개수 계산

--- src/main/java/penaltykick/domain/GameResult.java
+++ src/main/java/penaltykick/domain/GameResult.java
@@ -1,16 +1,22 @@
 package penaltykick.domain;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class GameResult {
 	private static final String GOAL_SUCCESS = "O";
 	private static final String GOAL_FAIL = "X";
-	// public final int goalCount;
+	public final int goalCount;
 	public final String convertResult;
 
 	public GameResult(List<Integer> computerPos, List<Integer> playerPos) {
 		this.convertResult = convertGameResult(computerPos, playerPos);
-		// this.goalCount = goalCount;
+		this.goalCount = countGoal(computerPos, playerPos);
+	}
+
+	private int countGoal(List<Integer> computerPos, List<Integer> playerPos) {
+		return (int)Arrays.stream(convertResult.split(""))
+			.filter(pos -> pos.contains(GOAL_SUCCESS)).count();
 	}
 
 	private String convertGameResult(List<Integer> computerPos, List<Integer> playerPos) {
