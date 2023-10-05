9a002f7b58f8f0defe206446a1a3bef0dbdb84b9
ParkDH0809 <znxn1237@gmail.com>
2023-10-05 23:48:12 +0900

    style(InputView): 입력 문구 변경

--- src/main/java/penaltykick/view/InputView.java
+++ src/main/java/penaltykick/view/InputView.java
@@ -6,18 +6,18 @@ import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
-    private static final String INPUT_NUMBER_PLAYER_1 = "플레이어1 숫자 입력";
-    private static final String INPUT_NUMBER_PLAYER_2 = "플레이어2 숫자 입력";
+    private static final String INPUT_NUMBER_PLAYER_1 = "플레이어1 : ";
+    private static final String INPUT_NUMBER_PLAYER_2 = "플레이어2 : ";
 
     Scanner scanner = new Scanner(System.in);
 
     public List<Integer> inputNumberPlayer1() {
-        System.out.println(INPUT_NUMBER_PLAYER_1);
+        System.out.print(INPUT_NUMBER_PLAYER_1);
         return InputValidation.validateInputValue(scanner.nextLine());
     }
 
     public List<Integer> inputNumberPlayer2() {
-        System.out.println(INPUT_NUMBER_PLAYER_2);
+        System.out.print(INPUT_NUMBER_PLAYER_2);
         return InputValidation.validateInputValue(scanner.nextLine());
     }
 }
