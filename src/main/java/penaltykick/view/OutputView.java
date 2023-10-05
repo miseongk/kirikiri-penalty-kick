dd492a8a1e5b2fbfbade1fbee081d56f498426c6
Hugh <lee2317884@gmail.com>
2023-10-05 18:13:32 +0900

    feat: 아웃풋 요소 상수지정

---
+++ src/main/java/penaltykick/view/OutputView.java
@@ -0,0 +1,21 @@
+package penaltykick.view;
+
+import penaltykick.domain.GameResult;
+
+public class OutputView {
+	private static final String GAME_START_FIRST_MESSAGE = "패널티 킥 게임을 시작합니다.";
+	private static final String GAME_START_SECOND_MESSAGE = "총 5번의 위치를 입력해주세요.";
+	private static final String GAME_CLEAR_MESSAGE = "게임 결과";
+	private static final String GAME_ENDED_TIE_MESSAGE = "무승부입니다.";
+	private static final String OUTPUT_PLAYER_ONE_MESSAGE = "플레이어1 : ";
+	private static final String OUTPUT_PLAYER_TWO_MESSAGE = "플레이어2 : ";
+	private static final String WINNER_PLAYER_ONE_MESSAGE = "플레이어1이(가) 우승했습니다\uD83C\uDF89";
+	private static final String WINNER_PLAYER_TWO_MESSAGE = "플레이어2이(가) 우승했습니다\uD83C\uDF89";
+	private final GameResult playerOneResult;
+	private final GameResult playerTwoResult;
+
+	public OutputView(GameResult playerOneResult, GameResult playerTwoResult) {
+		this.playerOneResult = playerOneResult;
+		this.playerTwoResult = playerTwoResult;
+	}
+}
