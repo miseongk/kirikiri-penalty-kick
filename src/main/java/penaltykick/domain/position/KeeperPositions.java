e82eca9153099c864baab97f7e872353a02d9f60
nhlee98 <e5slnh9876@gmail.com>
2023-10-06 21:02:12 +0900

    refactor: 사용하지 않는 메소드 제거

--- src/main/java/penaltykick/domain/position/KeeperPositions.java
+++ src/main/java/penaltykick/domain/position/KeeperPositions.java
@@ -1,22 +1,12 @@
 package penaltykick.domain.position;
 
 import java.util.List;
-import java.util.stream.Collectors;
-import penaltykick.domain.GoalResult;
 
 public final class KeeperPositions {
     private final PenaltyPositions keeperPositions;
 
     public KeeperPositions(PenaltyPositions penaltyPositions) {
         this.keeperPositions = penaltyPositions;
-    }
-
-    public KeeperPositions(List<Integer> selectedPositions) {
-        List<PostPosition> keeperPositions = selectedPositions.stream()
-                .map(PostPosition::valueOf)
-                .collect(Collectors.toList());
-
-        this.keeperPositions = new PenaltyPositions(keeperPositions);
     }
 
     public List<Boolean> matchPositions(BallPositions ballPositions) {
