23299f21d3a7845523227e6492eda50a42346d54
nhlee98 <e5slnh9876@gmail.com>
2023-10-06 18:15:46 +0900

    feat: 두 플레이어의 골 개수를 비교해 우승자를 구하는 기능 구현

--- src/main/java/penaltykick/service/GameService.java
+++ src/main/java/penaltykick/service/GameService.java
@@ -1,16 +1,29 @@
 package penaltykick.service;
 
-import java.util.List;
+import java.util.Comparator;
+import java.util.Optional;
+import java.util.stream.Stream;
+import penaltykick.domain.GameResult;
 import penaltykick.domain.GoalResult;
 
 public class GameService {
     private GameService() {}
 
-    public static int CountGoal(List<GoalResult> goalResults) {
-        long goalCount = goalResults.stream()
+    public static int countGoal(GameResult goalResult) {
+        long goalCount = goalResult.getGoalResults().stream()
                 .filter(GoalResult.GOAL::equals)
                 .count();
 
         return Math.toIntExact(goalCount);
+    }
+
+    public static Optional<String> getWinner(GameResult gameResult1, GameResult gameResult2) {
+        if (countGoal(gameResult1) == countGoal(gameResult2)) {
+            return Optional.empty();
+        }
+
+        return Stream.of(gameResult1, gameResult2)
+                .max(Comparator.comparingInt(GameService::countGoal))
+                .map(GameResult::getPlayerName);
     }
 }
