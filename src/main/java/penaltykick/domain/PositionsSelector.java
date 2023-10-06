f778440be86072fb8bef027544820b061e26cbda
nhlee98 <e5slnh9876@gmail.com>
2023-10-06 14:48:02 +0900

    style: 빠진 import 문 및 공백 추가

--- src/main/java/penaltykick/domain/PositionsSelector.java
+++ src/main/java/penaltykick/domain/PositionsSelector.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import penaltykick.domain.position.PenaltyPositions;
+import penaltykick.domain.position.PostPosition;
 
 public class PositionsSelector {
     private static final Random RANDOM = new Random();
@@ -14,7 +16,7 @@ public class PositionsSelector {
     public static PenaltyPositions selectPositions(int count) {
         return new PenaltyPositions(selectRandomPositions(count));
     }
-    
+
     private static List<PostPosition> selectRandomPositions(int count) {
         return IntStream.range(0, count)
                 .map(i -> RANDOM.nextInt(POSITIONS.length))
