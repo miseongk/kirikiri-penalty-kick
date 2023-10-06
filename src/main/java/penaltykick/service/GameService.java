c3ecb48cc4968d63e79a65433adbbe8e06decba1
nhlee98 <e5slnh9876@gmail.com>
2023-10-06 18:27:31 +0900

    feat: 게임 서비스 구현

--- src/main/java/penaltykick/service/GameService.java
+++ src/main/java/penaltykick/service/GameService.java
@@ -1,12 +1,25 @@
 package penaltykick.service;
 
 import java.util.Comparator;
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.Stream;
 import penaltykick.domain.GameResult;
 import penaltykick.domain.GoalResult;
+import penaltykick.domain.PositionsSelector;
+import penaltykick.domain.Round;
+import penaltykick.domain.position.BallPositions;
+import penaltykick.domain.position.KeeperPositions;
+import penaltykick.domain.position.PenaltyPositions;
 
 public class GameService {
+    private static KeeperPositions keeperPositions;
+
+    static {
+        int attemptsCount = PenaltyPositions.getAttemptsCount();
+        keeperPositions = new KeeperPositions(PositionsSelector.selectPositions(attemptsCount));
+    }
+
     private GameService() {}
 
     public static int countGoal(GameResult goalResult) {
@@ -15,6 +28,12 @@ public class GameService {
                 .count();
 
         return Math.toIntExact(goalCount);
+    }
+
+    public static GameResult startRound(List<Integer> BallPosition, String playerName) {
+        Round round = new Round(keeperPositions, new BallPositions(BallPosition));
+
+        return new GameResult(round.getGoalResult(), playerName);
     }
 
     public static Optional<String> getWinner(GameResult gameResult1, GameResult gameResult2) {
