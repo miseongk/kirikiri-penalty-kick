aabc180890d4d1c586c4c94d51a09a664c718ed7
nhlee98 <e5slnh9876@gmail.com>
2023-10-06 15:51:45 +0900

    feat: 공을 찰 위치가 공백으로 구분된 숫자가 아니라면 예외를 발생시키는 기능 구현

--- src/main/java/penaltykick/view/InputView.java
+++ src/main/java/penaltykick/view/InputView.java
@@ -15,9 +15,25 @@ public class InputView {
         System.out.printf("%s : ", name);
         String ballPositions = scanner.nextLine();
 
+        validateIsSeparatedNumber(ballPositions);
+
         return Arrays.stream(ballPositions.split(DELIMITER))
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
 
+    }
+
+    private static void validateIsSeparatedNumber(String input) {
+        if (!isSeparatedNumber(input)) {
+            throw new IllegalArgumentException(String.format("[ERROR] 공의 위치는 %s로 구분되는 숫자여야 합니다.", DELIMITER));
+        }
+    }
+
+    private static boolean isSeparatedNumber(String input) {
+        return !input.endsWith(DELIMITER) && Arrays.stream(input.split(DELIMITER)).allMatch(InputView::isNumber);
+    }
+
+    private static boolean isNumber(String string) {
+        return !string.isEmpty() && string.chars().allMatch(Character::isDigit);
     }
 }
