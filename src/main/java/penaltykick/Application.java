a15de444a7092918fb2d9775655a0d4aa093d008
nhlee98 <e5slnh9876@gmail.com>
2023-10-06 18:30:56 +0900

    feat: 어플리케이션 구현

--- src/main/java/penaltykick/Application.java
+++ src/main/java/penaltykick/Application.java
@@ -1,8 +1,9 @@
 package penaltykick;
 
-public class Application {
+import penaltykick.controller.GameController;
 
+public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        GameController.playGame();
     }
 }
