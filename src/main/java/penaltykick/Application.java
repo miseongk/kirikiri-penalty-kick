231f1166d8c9f050d3868ac3c41a4f7899ae41b4
yuseonjun <seonjun0906@gmail.com>
2023-10-07 17:46:22 +0900

    refactor: TODO 삭제

--- src/main/java/penaltykick/Application.java
+++ src/main/java/penaltykick/Application.java
@@ -7,7 +7,6 @@ import penaltykick.domain.RandomNumberGenerator;
 public class Application {
 
 	public static void main(String[] args) {
-		// TODO: 프로그램 구현
 		PenaltyKickController penaltyKickController = new PenaltyKickController(
 			new PenaltyKickReferee(new RandomNumberGenerator()));
 		penaltyKickController.run();
