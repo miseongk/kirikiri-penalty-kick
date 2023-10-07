612f7283bf95de29a17425cd8a4a8de661cc2330
benjaminuj <chosj1526@gmail.com>
2023-10-07 14:01:53 +0900

    feat: Implement a function to find a winner

--- src/main/java/penaltykick/Application.java
+++ src/main/java/penaltykick/Application.java
@@ -6,6 +6,8 @@ import java.io.InputStreamReader;
 
 public class Application {
     static Integer[] goalkeeperKick = new Integer[5];
+    static String resultOfPlayer1;
+    static String resultOfPlayer2;
 
     public static void main(String[] args) throws IOException {
         Integer[] player1Kick = new Integer[5];
@@ -20,10 +22,16 @@ public class Application {
 
         System.out.println("");
         System.out.println("게임 결과");
-        String resultOfPlayer1 = findOutIfIsGoal(player1Kick);
-        String resultOfPlayer2 = findOutIfIsGoal(player2Kick);
+        resultOfPlayer1 = findOutIfIsGoal(player1Kick);
+        resultOfPlayer2 = findOutIfIsGoal(player2Kick);
         System.out.print("플레이어1 : " + resultOfPlayer1 + "\n");
         System.out.print("플레이어2 : " + resultOfPlayer2 + "\n");
+        int winner = getWinner();
+        if(winner == 0) {
+            System.out.println("무승부입니다.");
+        } else {
+            System.out.println("플레이어" + winner + "이(가) 우승했습니다🎉");
+        }
     }
 
     /**
@@ -102,5 +110,32 @@ public class Application {
         }
 
         return result.toString();
+    }
+
+    /**
+     * 결과(우승자) 구하기
+     */
+    public static int getWinner() {
+        int count1 = 0;
+        int count2 = 0;
+
+        for (int i = 0; i < 5; i++) {
+            char result1 = resultOfPlayer1.charAt(i);
+            if (result1 == 'O') {
+                count1++;
+            }
+
+            char result2 = resultOfPlayer2.charAt(i);
+            if (result2 == 'O') {
+                count2++;
+            }
+        }
+
+        if (count1 > count2) {
+            return 1;
+        } else if (count1 < count2) {
+            return 2;
+        }
+        return 0;
     }
 }
