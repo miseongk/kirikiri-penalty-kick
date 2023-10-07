94a713b243a3602cc4ba457e1df804c1a87a1338
benjaminuj <chosj1526@gmail.com>
2023-10-07 13:34:19 +0900

    fix: Modify the array to include player kick values as numeric values

--- src/main/java/penaltykick/Application.java
+++ src/main/java/penaltykick/Application.java
@@ -5,58 +5,79 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Application {
-    static String[] player1Kick;
-    static String[] player2Kick;
     static Integer[] goalkeeperKick = new Integer[5];
 
     public static void main(String[] args) throws IOException {
         String result1;
         String result2;
+        Integer[] player1Kick = new Integer[5];
+        Integer[] player2Kick = new Integer[5];
 
         System.out.println("패널티 킥 게임을 시작합니다.");
 
         makeGoalkeeperKick();
 
         System.out.println("총 5번의 위치를 입력해주세요.");
-        getPlayerLocation();
+        getPlayerLocation(player1Kick, player2Kick);
 
         System.out.println("");
         System.out.println("게임 결과");
 
     }
 
-    public static void getPlayerLocation() throws IOException {
+    /**
+     * 골키퍼의 패널티 킥 위치 랜덤 생성
+     */
+    public static void makeGoalkeeperKick() {
+        for(int i = 0; i < 5; i++) {
+            goalkeeperKick[i] = (int)((Math.random() * 3) + 1);
+        }
+    }
+
+    /**
+     * 플레이어1과 플레이어2의 패널티 킥 위치 입력받기
+     */
+    public static void getPlayerLocation(Integer[] player1Kick, Integer[] player2Kick) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] player1KickTemp;
+        String[] player2KickTemp;
 
         System.out.print("플레이어1 : ");
         String input1 = br.readLine();
 
         try {
-            player1Kick = input1.split(" ");
+            player1KickTemp = input1.split(" ");
         } catch (Exception exception) {
             IllegalArgumentException e = new IllegalArgumentException("예외 발생시 IllegalArgumentException으로 통합");
             throw e;
         }
-        checkIfValueIsNormal(player1Kick);
+        checkIfValueIsNormal(player1KickTemp, player1Kick);
 
         System.out.print("플레이어2 : ");
         String input2 = br.readLine();
         try {
-            player2Kick = input2.split(" ");
+            player2KickTemp = input2.split(" ");
         } catch (Exception exception) {
             IllegalArgumentException e = new IllegalArgumentException("예외 발생시 IllegalArgumentException으로 통합");
             throw e;
         }
-        checkIfValueIsNormal(player2Kick);
+        checkIfValueIsNormal(player2KickTemp, player2Kick);
     }
 
-    public static void checkIfValueIsNormal(String[] kickArray) {
+    /**
+     * 입력된 값이 정상적인지 검사 : 잘못된 값을 입력할 경우 IllegalArgumentException을 발생시킨 후 프로그램 종료시키기
+     */
+    public static void checkIfValueIsNormal(String[] tempKicks, Integer[] kicks) {
+        int index = 0;
         try {
-            for (String kick : kickArray) {
+            for (String kick : tempKicks) {
                 int kickNumber = Integer.parseInt(kick);
-                if ((kickNumber<1) || (kickNumber>3)) {
+                if ((kickNumber < 1) || (kickNumber > 3)) {
                     IllegalArgumentException e = new IllegalArgumentException("예외 발생시 IllegalArgumentException으로 통합");
                     throw e;
+                } else {
+                    kicks[index++] = kickNumber;
                 }
             }
         } catch (NumberFormatException numberFormatException) {
@@ -65,9 +86,10 @@ public class Application {
         }
     }
 
-    public static void makeGoalkeeperKick() {
-        for(int i=0; i<5; i++) {
-            goalkeeperKick[i] = (int)((Math.random() * 3) + 1);
-        }
+    /**
+     * 골 여부 구하기
+     */
+    public static void findOutIfIsGoal() {
+
     }
 }
