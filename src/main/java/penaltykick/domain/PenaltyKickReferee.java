5f314bff4a85b65af8d982946ca114b1f09b0f1e
yuseonjun <seonjun0906@gmail.com>
2023-10-07 20:04:33 +0900

    refactor: 결과 계산 로직 Computer 클래스로 이동

--- src/main/java/penaltykick/domain/PenaltyKickReferee.java
+++ src/main/java/penaltykick/domain/PenaltyKickReferee.java
@@ -1,28 +1,12 @@
 package penaltykick.domain;
 
-import java.util.Arrays;
+import java.util.List;
 
 public class PenaltyKickReferee {
 
-	private static final String ALPHABET_O = "O";
-	private static final String ALPHABET_X = "X";
-
-	public String calculateResult(Computer computer, Player player) {
-		StringBuilder stringBuilder = new StringBuilder();
-		for (int i = 0; i < computer.getComputerNumberSize(); i++) {
-			if (computer.checkNumberByPlayer(player.getPlayerNumberByIndex(i), i)) {
-				stringBuilder.append(ALPHABET_O);
-			}
-			if (!computer.checkNumberByPlayer(player.getPlayerNumberByIndex(i), i)) {
-				stringBuilder.append(ALPHABET_X);
-			}
-		}
-		return stringBuilder.toString();
-	}
-
-	public long resultCount(String result) {
-		return Arrays.stream(result.split(""))
-			.filter(element -> element.contains(ALPHABET_O))
+	public long resultCount(List<Boolean> results) {
+		return results.stream()
+			.filter(result -> result.equals(true))
 			.count();
 	}
 }
