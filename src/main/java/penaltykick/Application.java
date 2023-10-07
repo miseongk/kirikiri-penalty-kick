24d919463688675632a9c4fda35580e51bdc99ff
benjaminuj <chosj1526@gmail.com>
2023-10-07 13:16:47 +0900

    feat: Create a function to randomly generate the goalkeeper's penalty kick location

--- src/main/java/penaltykick/Application.java
+++ src/main/java/penaltykick/Application.java
@@ -7,16 +7,22 @@ import java.io.InputStreamReader;
 public class Application {
     static String[] player1Kick;
     static String[] player2Kick;
+    static Integer[] goalkeeperKick = new Integer[5];
 
     public static void main(String[] args) throws IOException {
         String result1;
         String result2;
-        System.out.print("패널티 킥 게임을 시작합니다.\n총 5번의 위치를 입력해주세요.\n");
 
+        System.out.println("패널티 킥 게임을 시작합니다.");
+
+        makeGoalkeeperKick();
+
+        System.out.println("총 5번의 위치를 입력해주세요.");
         getPlayerLocation();
 
         System.out.println("");
         System.out.println("게임 결과");
+
     }
 
     public static void getPlayerLocation() throws IOException {
@@ -48,7 +54,7 @@ public class Application {
         try {
             for (String kick : kickArray) {
                 int kickNumber = Integer.parseInt(kick);
-                if ((kickNumber < 1) || (kickNumber > 3)) {
+                if ((kickNumber<1) || (kickNumber>3)) {
                     IllegalArgumentException e = new IllegalArgumentException("예외 발생시 IllegalArgumentException으로 통합");
                     throw e;
                 }
@@ -56,6 +62,12 @@ public class Application {
         } catch (NumberFormatException numberFormatException) {
             IllegalArgumentException e = new IllegalArgumentException("예외 발생시 IllegalArgumentException으로 통합");
             throw e;
+        }
+    }
+
+    public static void makeGoalkeeperKick() {
+        for(int i=0; i<5; i++) {
+            goalkeeperKick[i] = (int)((Math.random() * 3) + 1);
         }
     }
 }
