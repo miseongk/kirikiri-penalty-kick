26ae248598a603c7ee5d9920598d7a664e63406b
SehyeonKang <gedfgrdt@naver.com>
2023-10-07 12:12:29 +0900

    feat(Player): 플레이어 클래스 추가

---
+++ src/main/java/penaltykick/domain/Player.java
@@ -0,0 +1,56 @@
+package penaltykick.domain;
+
+import java.util.List;
+
+public class Player {
+
+    private String name;
+    private List<Integer> penaltyKickPosition;
+    private int score;
+
+    public Player(int playerNum, List<Integer> penaltyKickPosition) {
+        validateKickPosition(penaltyKickPosition);
+        this.name = "플레이어" + playerNum;
+        this.penaltyKickPosition = penaltyKickPosition;
+        this.score = 0;
+    }
+
+    private void validateKickPosition(List<Integer> penaltyKickPosition) {
+        for (int position : penaltyKickPosition) {
+            if (position < 1 || position > 3) {
+                throw new IllegalArgumentException("패널티 킥 위치는 1이상 3이하의 정수여야 합니다.");
+            }
+        }
+    }
+
+    public void progressPenaltyKick(GoalKeeper goalKeeper) {
+        List<Integer> goalKeeperDefensePosition = goalKeeper.getDefensePosition();
+
+        for (int i = 0; i < 5; i++) {
+            if (penaltyKickPosition.get(i) != goalKeeperDefensePosition.get(i)) {
+                score++;
+            }
+        }
+    }
+
+    public boolean isVictory(Player competitor) {
+        int competitorScore = competitor.getScore();
+
+        if (score > competitorScore) {
+            return true;
+        }
+        return false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Integer> getPenaltyKickPosition() {
+        return penaltyKickPosition;
+    }
+
+    public int getScore() {
+        return score;
+    }
+}
