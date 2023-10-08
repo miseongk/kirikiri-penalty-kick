560e4eeeb62fd1b57e8314b6346162dbc479d71d
Charm-Star <zxzx05050@naver.com>
2023-10-08 16:19:53 +0900

    미션2 완료

--- src/main/java/penaltykick/utill/Validation.java
+++ src/main/java/penaltykick/utill/Validation.java
@@ -4,27 +4,25 @@ import penaltykick.domain.Player;
 
 import java.util.List;
 
-import static penaltykick.utill.Reference.ERROR_MESSAGE;
-import static penaltykick.utill.Reference.INPUT_COUNT;
+import static penaltykick.utill.Reference.*;
 
 public class Validation {
 
-    public boolean checkRange(List<Integer> list) {
+    public boolean isValidNumber(List<Integer> list) {
         for (int n : list)
-            if (n > 3 || n < 1) {
+            if (n > MAXIMUM_NUMBER || n < MINIMUM_NUMBER) {
                 return false;
             }
         return true;
     }
-
-    public boolean checkLength(List<Integer> list) {
+    public boolean isValidLength(List<Integer> list) {
 
         return list.size() == INPUT_COUNT;
     }
 
     public boolean isValid(List<Player> playerInput) {
         for (Player player : playerInput) {
-            if(!checkRange(player.getDirectionList()) || !checkLength(player.getDirectionList())){
+            if(!isValidNumber(player.getDirectionList()) || !isValidLength(player.getDirectionList())){
                 return false;
             }
         }
