2764bb664c71985480743b8cb1642cd01fa505a1
yuseonjun <seonjun0906@gmail.com>
2023-10-08 15:42:17 +0900

    refactor: 의존성 주입 추가

--- src/main/java/penaltykick/Application.java
+++ src/main/java/penaltykick/Application.java
@@ -7,8 +7,8 @@ import penaltykick.domain.RandomNumberGenerator;
 public class Application {
 
 	public static void main(String[] args) {
-		PenaltyKickController penaltyKickController = new PenaltyKickController(
-			new PenaltyKickReferee(new RandomNumberGenerator()));
+		PenaltyKickController penaltyKickController = new PenaltyKickController(new PenaltyKickReferee(),
+			new RandomNumberGenerator());
 		penaltyKickController.run();
 	}
 }
