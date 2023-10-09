be4146b44409da28bf7106bc774f169fbdf994fb
jongheonleee <qwefghnm1212@gmail.com>
2023-10-09 11:27:04 +0900

    플레이어 리팩토링 - 의존성 주입, 검증 메서드, 크기 확인

--- src/main/java/penaltykick/domain/Goalkeeper.java
+++ src/main/java/penaltykick/domain/Goalkeeper.java
@@ -16,22 +16,16 @@ public class Goalkeeper {
     }
 
     public void generateNumbers() {
-        validate();
+        checkNumbersIsNotEmpty();
         for (int i=0; i<LIMIT; i++) {
             int randomNumber = generateRandom.getNumber();
             numbers.add(randomNumber);
         }
     }
 
-    // validate : check if goalkeeper has random numbers
-    private void validate() {
-        if (getSize() != 0) {
-            throw new IllegalStateException("이미 골키퍼가 숫자를 생성했습니다");
-        }
-    }
 
     public int getNumber(int index) {
-        if (getSize() == 0) throw new IllegalStateException("골키퍼가 아직 숫자를 생성하지 않았습니다");
+        checkNumbersIsEmpty();
         return numbers.get(index);
     }
 
@@ -43,4 +37,15 @@ public class Goalkeeper {
         return numbers.size();
     }
 
+    private void checkNumbersIsNotEmpty() {
+        if (getSize() != 0) {
+            throw new IllegalStateException("이미 골키퍼가 숫자를 생성했습니다");
+        }
+    }
+
+    private void checkNumbersIsEmpty() {
+        if (getSize() == 0) {
+            throw new IllegalStateException("골키퍼가 아직 숫자를 생성하지 않았습니다")
+        }
+    }
 }
--- src/main/java/penaltykick/domain/Player.java
+++ src/main/java/penaltykick/domain/Player.java
@@ -14,7 +14,8 @@ public class Player {
     }
 
     public void generateNumbers(String input) {
-        if (!isValidFormatAndRange(input)) throw new IllegalArgumentException("잘못된 입력형식입니다.");
+        checkNumbersIsNotEmpty();
+        checkValidInput(input);
 
         for (int i=0; i<input.length(); i+=2) {
             int number = input.charAt(i) - '0';
@@ -22,24 +23,9 @@ public class Player {
         }
     }
 
-    private boolean isValidFormatAndRange(String input) {
-        for (int i=0; i<input.length(); i++) {
-            char token = input.charAt(i);
-
-            if (i%2 == 0) {
-                int number = token - '0';
-                if (!(START <= number && number <= END)) return false;
-            } else {
-                if (token != ' ') {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
 
     public int getNumber(int index) {
-        if (numbers.size() == 0) throw new IllegalStateException("플레이어가 아직 숫자를 생성하지 않았습니다");
+        checkNumbersIsEmpty();
         return this.numbers.get(index);
     }
 
@@ -49,5 +35,49 @@ public class Player {
 
     public String getName() {
         return this.name;
+    }
+
+    public int getSize() {
+        return numbers.size();
+    }
+
+    private void checkValidInput(String input) {
+        for (int i=0; i<input.length(); i++) {
+            char token = input.charAt(i);
+            checkValidToken(i, token);
+        }
+    }
+
+    private void checkValidToken(Integer idx, Character token) {
+        if (idx%2 == 0) {
+            checkValidNumber(token);
+        } else {
+            checkValidSpace(token);
+        }
+    }
+
+    private void checkValidNumber(Character token) {
+        Integer number = token - '0';
+        if (!(START <= number && number <= END)) {
+            throw new IllegalArgumentException("잘못된 입력형식입니다.");
+        }
+    }
+
+    private void checkValidSpace(Character token) {
+        if (token != ' ') {
+            throw new IllegalArgumentException("잘못된 입력형식입니다.");
+        }
+    }
+
+    private void checkNumbersIsNotEmpty() {
+        if (getSize() != 0) {
+            throw new IllegalStateException("이미 플레이어가 숫자를 생성했습니다");
+        }
+    }
+
+    private void checkNumbersIsEmpty() {
+        if (getSize() == 0) {
+            throw new IllegalStateException("플레이어가 아직 숫자를 생성하지 않았습니다")
+        }
     }
 }
