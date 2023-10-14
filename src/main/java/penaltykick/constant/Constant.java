751b0de9ef54e05eb2e3cabf9209fd6e8fe447ff
Hugh <lee2317884@gmail.com>
2023-10-14 23:42:20 +0900

    feat: 공통 상수 Constant 클래스에 통합

---
+++ src/main/java/penaltykick/constant/Constant.java
@@ -0,0 +1,7 @@
+package penaltykick.constant;
+
+public class Constant {
+	public static final int POSITION_LENGTH = 5;
+	public static final int START_RANGE = 1;
+	public static final int END_RANGE = 3;
+}
