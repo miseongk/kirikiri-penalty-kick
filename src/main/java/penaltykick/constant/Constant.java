28c0af049800b37989cddb6a1720f97f9b406cc9
Hugh <lee2317884@gmail.com>
2023-10-15 03:27:28 +0900

    feat: Blank 상수 추가

--- src/main/java/penaltykick/constant/Constant.java
+++ src/main/java/penaltykick/constant/Constant.java
@@ -4,4 +4,5 @@ public class Constant {
 	public static final int POSITION_LENGTH = 5;
 	public static final int START_RANGE = 1;
 	public static final int END_RANGE = 3;
+	public static final String BLANK_VALUE = "";
 }
