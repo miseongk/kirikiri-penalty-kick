77e2462e1197762c4961d9b21ea0e7fbef827bf6
Hugh <lee2317884@gmail.com>
2023-10-15 17:11:59 +0900

    refactor: SecureRandom 적용

--- src/main/java/penaltykick/domain/ComputerPosition.java
+++ src/main/java/penaltykick/domain/ComputerPosition.java
@@ -1,28 +1,22 @@
 package penaltykick.domain;
 
+import java.security.SecureRandom;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
 import java.util.stream.Collectors;
 
 import penaltykick.constant.Constant;
 
 public class ComputerPosition {
+	private static final SecureRandom random = new SecureRandom();
 
-	private final List<Integer> computerPosition;
-	private final Random random = new Random();
-
-	public ComputerPosition() {
-		this.computerPosition = pickRandomPosition();
-	}
-
-	private List<Integer> pickRandomPosition() {
+	private static List<Integer> pickRandomPosition() {
 		return Arrays.stream(new List[Constant.POSITION_LENGTH])
 			.map(num -> random.nextInt(Constant.END_RANGE) + Constant.START_RANGE)
 			.collect(Collectors.toList());
 	}
 
 	public List<Integer> getComputerPosition() {
-		return computerPosition;
+		return pickRandomPosition();
 	}
 }
